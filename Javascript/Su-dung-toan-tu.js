76906a579328952416d40c334ecfda1fd11af58c
unknown <shuhari0123@gmail.com>
2023-09-11 15:05:35 +0700

    su-dung-toan-tu

---
+++ Javascript/Su-dung-toan-tu.js
@@ -0,0 +1,8 @@
+ let inputWidth;
+      let inputHeight;
+      inputWidth = prompt("Enter the width");
+      inputHeight = prompt("Enter the Height");
+      let Width = parseInt(inputWidth);
+      let Height = parseInt(inputHeight);
+      let area = Width * Height;
+      document.write("The area is " + area);
